0de402b215b2a72d8d38db61a9165c73422867ab
trax <o.givernaud@gmail.com>
2014-03-10 22:30:17 +0100

    skel for cmake

---
+++ src/Img.cpp
@@ -0,0 +1,16 @@
+#include "Img.hpp"
+
+namespace dotray {
+
+namespace img {
+
+Img::Img(W w, H h, Type t) :
+  w(w),
+  h(h),
+  type(t){
+  
+}
+
+} // img
+
+} // dotray
---
+++ src/utils/commonTypes.hpp
@@ -0,0 +1,31 @@
+#ifndef DOTARAY_COMMONTYPES_H_
+#define DOTARAY_COMMONTYPES_H_
+
+#include <cstdint>
+#include <boost/serialization/strong_typedef.hpp>
+
+using std::uint64_t;
+using std::uint32_t;
+using std::uint16_t;
+using std::uint8_t;
+
+using std::int64_t;
+using std::int32_t;
+using std::int16_t;
+using std::int8_t;
+
+BOOST_STRONG_TYPEDEF(uint16_t, W);
+BOOST_STRONG_TYPEDEF(uint16_t, H);
+
+
+#endif /* DOTARAY */
+
+
+
+
+
+
+
+
+
+
