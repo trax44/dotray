7ca85f3eda73defff424cfa6dbf7576a19d66b3b
trax <o.givernaud@gmail.com>
2014-03-15 01:08:26 +0100

    Adding vector construction from two points

--- src/math/Vector.cpp
+++ src/math/Vector.cpp
@@ -1,44 +1,69 @@
 #include "Vector.hpp"
 namespace dotray {
-    namespace math {
-        namespace vector {
+namespace math {
+namespace vector {
 
-            Vector::Vector(const X x, const Y y, const Z z) : x(x), y(y), z(z) {};
-            Vector::~Vector() {};
+Vector::Vector(const X x, 
+               const Y y, 
+               const Z z) : 
+  x(x), y(y), z(z) {};
 
-            X Vector::getX() const {
-                return x;
-            }
+Vector::Vector(const Vector &pointA,
+               const Vector &pointB):
+  x(pointB.getX() - pointB.getX()),
+  y(pointB.getY() - pointB.getY()),
+  z(pointB.getZ() - pointB.getZ()){}
+  
 
-            Y Vector::getY() const {
-                return y;
-            }
+X Vector::getX() const {
+  return x;
+}
 
-            Z Vector::getZ() const {
-                return z;
-            }
+Y Vector::getY() const {
+  return y;
+}
 
-            const int operator*(const Vector & lhs, const Vector & rhs) {
-                return lhs.getX() * rhs.getX() + lhs.getY() * rhs.getY() + lhs.getZ() * rhs.getZ();
-            }
-            const Vector operator*(const int & lhs, const Vector & rhs) {
-              return Vector(X(lhs*rhs.getX()), Y(lhs*rhs.getY()), Z(lhs*rhs.getZ()));
-            }
-            const Vector operator*(const Vector & lhs, const int & rhs) {
-                return Vector(X(lhs.getX()*rhs), Y(lhs.getY()*rhs), Z(lhs.getZ()*rhs));
-            }
-            const Vector operator^(const Vector & lhs, const Vector & rhs) {
-                return Vector(X(lhs.getY()*rhs.getZ()-lhs.getZ()*rhs.getY()), Y(lhs.getZ()*rhs.getX()-lhs.getX()*rhs.getZ()), Z(lhs.getX()*rhs.getY()-lhs.getY()*rhs.getX()));
-            }
-            const Vector operator+(const Vector & lhs, const Vector & rhs) {
-                return Vector(X(lhs.getX()+rhs.getX()), Y(lhs.getY()+rhs.getY()), Z(lhs.getZ()+rhs.getZ()));
-            }
-            const Vector operator-(const Vector & lhs, const Vector & rhs) {
-                return Vector(X(lhs.getX()-rhs.getX()), Y(lhs.getY()-rhs.getY()), Z(lhs.getZ()-rhs.getZ()));
-            }
-            const bool operator==(const Vector & lhs, const Vector & rhs) {
-                return lhs.getX() == rhs.getX() && lhs.getY() == rhs.getY() && lhs.getZ() == rhs.getZ(); 
-            }
-        } //vector
-    } //math
+Z Vector::getZ() const {
+  return z;
+}
+
+const int operator*(const Vector & lhs, const Vector & rhs) {
+  return 
+    lhs.getX() * rhs.getX() + 
+    lhs.getY() * rhs.getY() + 
+    lhs.getZ() * rhs.getZ();
+}
+const Vector operator*(const int & lhs, const Vector & rhs) {
+  return Vector(X(lhs*rhs.getX()), 
+                Y(lhs*rhs.getY()), 
+                Z(lhs*rhs.getZ()));
+}
+const Vector operator*(const Vector & lhs, const int & rhs) {
+  return Vector(X(lhs.getX()*rhs), 
+                Y(lhs.getY()*rhs), 
+                Z(lhs.getZ()*rhs));
+}
+const Vector operator^(const Vector & lhs, const Vector & rhs) {
+  return Vector(X(lhs.getY()*rhs.getZ()-lhs.getZ()*rhs.getY()), 
+                Y(lhs.getZ()*rhs.getX()-lhs.getX()*rhs.getZ()), 
+                Z(lhs.getX()*rhs.getY()-lhs.getY()*rhs.getX()));
+}
+const Vector operator+(const Vector & lhs, const Vector & rhs) {
+  return Vector(X(lhs.getX()+rhs.getX()), 
+                Y(lhs.getY()+rhs.getY()), 
+                Z(lhs.getZ()+rhs.getZ()));
+}
+const Vector operator-(const Vector & lhs, const Vector & rhs) {
+  return Vector(X(lhs.getX()-rhs.getX()), 
+                Y(lhs.getY()-rhs.getY()), 
+                Z(lhs.getZ()-rhs.getZ()));
+}
+const bool operator==(const Vector & lhs, const Vector & rhs) {
+  return 
+    lhs.getX() == rhs.getX() && 
+    lhs.getY() == rhs.getY() && 
+    lhs.getZ() == rhs.getZ(); 
+}
+} //vector
+} //math
 } //dotray
--- src/math/Vector.hpp
+++ src/math/Vector.hpp
@@ -4,30 +4,30 @@
 #include "utils/commonTypes.hpp"
 
 namespace dotray {
-    namespace math {
-        namespace vector {
+namespace math {
+namespace vector {
 
-            class Vector {             
-                X x;
-                Y y;
-                Z z;
-            public :
-                Vector(X x, Y y, Z z);
-                ~Vector();
-                X getX() const;
-                Y getY() const;
-                Z getZ() const;
-            };
+class Vector {             
+  X x;
+  Y y;
+  Z z;
+public :
+  Vector(const Vector &pointA, const Vector &pointB);
+  Vector(const X x, const Y y, const Z z);
+  X getX() const;
+  Y getY() const;
+  Z getZ() const;
+};
                 
-            const int operator*(const Vector & lhs, const Vector & rhs);
-            const Vector operator*(const int & lhs, const Vector & rhs);
-            const Vector operator*(const Vector & lhs, const int & rhs);
-            const Vector operator^(const Vector & lhs, const Vector & rhs);
-            const Vector operator+(const Vector & lhs, const Vector & rhs);
-            const Vector operator-(const Vector & lhs, const Vector & rhs);
-            const bool operator==(const Vector & lhs, const Vector & rhs);
-        } //vector
-    } //math
+const int operator*(const Vector & lhs, const Vector & rhs);
+const Vector operator*(const int & lhs, const Vector & rhs);
+const Vector operator*(const Vector & lhs, const int & rhs);
+const Vector operator^(const Vector & lhs, const Vector & rhs);
+const Vector operator+(const Vector & lhs, const Vector & rhs);
+const Vector operator-(const Vector & lhs, const Vector & rhs);
+const bool operator==(const Vector & lhs, const Vector & rhs);
+} //vector
+} //math
 } //dotray
 
 #endif /* DOTRAY_MATH_VECTOR_H_ */
