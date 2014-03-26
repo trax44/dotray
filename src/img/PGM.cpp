c46a7a24c9b4d89a6366983a2b7c2b8adda51222
trax <o.givernaud@gmail.com>
2014-03-26 17:44:05 +0100

    PGM normalize image intensity before dumping it

--- src/img/PGM.cpp
+++ src/img/PGM.cpp
@@ -1,5 +1,7 @@
 #include "PGM.hpp"
 
+#include <algorithm>
+
 namespace dotray {
 
 namespace img {
@@ -7,34 +9,32 @@ namespace img {
 
 
 bool PGM::save (const std::string &filename, const Img<GREY_255> &img) {
-  std::ofstream myfile;
-  myfile.open (filename);
-  const bool res (myfile.is_open());
-  if (!res){
-    return false;
-  }
+std::ofstream myfile;
+ myfile.open (filename);
+ const bool res (myfile.is_open());
+ if (!res){
+   return false;
+ }
 
-  myfile << "P2\n";
-  myfile << std::to_string(img.getWidth()) << " " 
-         << std::to_string(img.getHeight()) << "\n";
+ myfile << "P2\n";
+ myfile << std::to_string(img.getWidth()) << " " 
+        << std::to_string(img.getHeight()) << "\n";
 
-  myfile << "255\n";
+ myfile << "255\n";
 
-  int pixCount = 0;
-  for (auto 
-         it  =  img.cbegin() ,
-         end   = img.cend() ; it != end ; ++it){
-    myfile << std::to_string (*it) << " ";
-    ++pixCount;
-    if ((pixCount % img.getWidth()) == 0){
-      myfile << "\n";
-    }
-    
-  }
-  myfile << "\n";
+ int pixCount = 0;
+ for (auto it  =  img.cbegin() , end = img.cend() ; 
+      it != end ; ++it){
+   myfile << std::to_string (*it) << " ";
+   ++pixCount;
+   if ((pixCount % img.getWidth()) == 0){
+     myfile << "\n";
+   }
+ }
+ myfile << "\n";
 
-  myfile.close();
-  return true;
+ myfile.close();
+ return true;
 }
 
 bool PGM::save (const std::string &filename, const Img<RGB_3_255> &img) {
@@ -45,19 +45,39 @@ bool PGM::save (const std::string &filename, const Img<RGB_3_255> &img) {
     return false;
   }
 
+  float intensityMax = 0;
+  float intensityMin = 65535;
+  float minColor = 1;
+  for(auto it = img.cbegin(), end = img.cend() ;
+      it != end ; ++it) {
+    intensityMax = std::max(it->intensity, intensityMax);
+    intensityMin = std::min(it->intensity, intensityMin);
+  }
+  
+  if (intensityMax == 0){
+    intensityMax = 65535;
+  }
+  const float normalizeFactory (65535/(255*(intensityMax-intensityMin)));
+
+  std::cout << "intensity range " << intensityMin 
+            << " " << intensityMax 
+            << " " << normalizeFactory 
+            << " " << minColor
+            << std::endl;
+
   myfile << "P3\n";
   myfile << std::to_string(img.getWidth()) << " " 
          << std::to_string(img.getHeight()) << "\n";
 
-  myfile << "255\n";
+  myfile << "65535\n";
 
   int pixCount = 0;
   for (auto 
          it  =  img.cbegin() ,
          end   = img.cend() ; it != end ; ++it){
-    myfile << std::to_string (it->R) << " "
-           << std::to_string (it->G) << " "
-           << std::to_string (it->B) << "  ";
+    myfile << std::to_string (int32_t((it->R*it->intensity - intensityMin)*normalizeFactory)) << " "
+           << std::to_string (int32_t((it->G*it->intensity - intensityMin)*normalizeFactory)) << " "
+           << std::to_string (int32_t((it->B*it->intensity - intensityMin)*normalizeFactory)) << "  ";
     ++pixCount;
     if ((pixCount % img.getWidth()) == 0){
       myfile << "\n";
